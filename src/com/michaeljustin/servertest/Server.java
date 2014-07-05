efcc72841567121982b6ca3ac201ee1272fdd1fa
codewalker1337 <jrgolden2011@live.com>
2014-07-05 03:02:18 -0400

    Added Server.java fuck me like a french fry

--- src/com/michaeljustin/servertest/Server.java
+++ src/com/michaeljustin/servertest/Server.java
@@ -10,57 +10,59 @@ public class Server {
 	private Socket socket;
 	private ServerSocket serverSocket;
 	private boolean running;
-	
+
 	public Server(int PORT, int maxConnections) {
 		this.PORT = PORT;
 		this.maxConnections = maxConnections;
 	}
-	
+
 	public void startServer() throws IOException {
-	
 		serverSocket = new ServerSocket(PORT);
 		setRunning(true);
-		while(running) {
+		while (running) {
 			socket = serverSocket.accept();
-		}	
+		}
 	}
-	
+
 	public void StopServer(int exitValue) throws IOException {
-		//TODO close sockets and disconnect users
+		// TODO close sockets and disconnect users
 		socket.close();
 		serverSocket.close();
 		System.out.println("Sockets closed");
 		System.exit(exitValue);
 	}
-	
-	
-	//getters/setters
-	
+
+	// getters/setters
 	public int getPort() {
 		return PORT;
 	}
-	
+
 	public void setPort(int PORT) {
 		this.PORT = PORT;
 	}
-	
+
 	public int getMaxConnections() {
 		return maxConnections;
 	}
+
 	public void setMaxConnections(int maxConnections) {
 		this.maxConnections = maxConnections;
 	}
+
 	public Socket getSocket() {
 		return socket;
 	}
+
 	public ServerSocket getServerSocket() {
 		return serverSocket;
 	}
+
 	public boolean isRunning() {
 		return running;
 	}
+
 	public void setRunning(boolean running) {
 		this.running = running;
 	}
-	
+
 }
