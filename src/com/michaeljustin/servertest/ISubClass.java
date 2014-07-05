0c88bfc01ebf8330f731629f0159e68fb483c8f8
mikebjava <Michael@10.0.0.15>
2014-07-05 01:55:18 -0400

    Added a initialization system.

---
+++ src/com/michaeljustin/servertest/ISubClass.java
@@ -0,0 +1,10 @@
+package com.michaeljustin.servertest;
+
+public interface ISubClass
+{
+	public void preInit();
+
+	public void init();
+
+	public void postInit();
+}
--- src/com/michaeljustin/servertest/Main.java
+++ src/com/michaeljustin/servertest/Main.java
@@ -1,11 +1,48 @@
 package com.michaeljustin.servertest;
 
-public class Main
+import java.util.ArrayList;
+
+public final class Main
 {
+
+	private static ArrayList<ISubClass> subClasses = new ArrayList<ISubClass>();
 
 	public static void main(String[] args)
 	{
+		if (subClasses.size() > 0)
+		{
+			for (int i = 0; i < subClasses.size(); i++)
+			{
+				if (subClasses instanceof ISubClass)
+				{
+					subClasses.get(i).preInit();
+				}
+			}
 
+			for (int i = 0; i < subClasses.size(); i++)
+			{
+				if (subClasses instanceof ISubClass)
+				{
+					subClasses.get(i).init();
+				}
+			}
+
+			for (int i = 0; i < subClasses.size(); i++)
+			{
+				if (subClasses instanceof ISubClass)
+				{
+					subClasses.get(i).postInit();
+				}
+			}
+		} else
+		{
+			System.out.println("No sub classes found to call.");
+		}
+	}
+
+	public static void addSubClass(ISubClass subclass)
+	{
+		subClasses.add(subclass);
 	}
 
 }
