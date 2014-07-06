f2cfca8dd2791c7bd7b4d08f2840a0b29ae754d6
mikebjava <Michael@10.0.0.15>
2014-07-05 22:00:15 -0400

    hgjhgjhjg

--- src/com/michaeljustin/servertest/Main.java
+++ src/com/michaeljustin/servertest/Main.java
@@ -1,11 +1,22 @@
 package com.michaeljustin.servertest;
 
+import java.io.IOException;
+
 public class Main
 {
 
 	public static void main(String[] args)
 	{
-		new Server(1234, 5).addServerHandler(new ExampleServerHandler());
+		Server server  =new Server(1234, 5);
+		server.addServerHandler(new ExampleServerHandler());
+		try
+		{
+			server.startServer();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
